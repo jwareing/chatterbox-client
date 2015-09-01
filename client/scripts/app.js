2f649acca00493db88513acd838d2a38b2658eca
jwareing <jwareing89@gmail.com>
2015-09-01 11:13:36 -0700

    Fixed bugs in room selection and creation. Added friend features.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   // Submit message on button click
   $('.button').on('click',function(){
+    if (app.currentRoom === "NEW ROOM"){
+      app.currentRoom = prompt("What is the name of your new room?");
+    }
+
     var myMessage = $('#message').val();
     var message = {
       username: userName,
       text: myMessage,
-      roomname: '4chan'
+      roomname: app.currentRoom
     };
     app.send(message);
   });
@@ -19,14 +23,19 @@ $(document).ready(function() {
       return false;
     }
   });
-$('select').on( "change", function(){
-  app.currentRoom = this.value;
-  app.displayMostRecent(app.currentRoom);
-  app.roomListUpdate();
-  console.log(app.currentRoom);
 
-});
+  $('select').on( "change", function(){
+    app.currentRoom = this.value;
+    app.displayMostRecent(app.currentRoom);
+    app.roomListUpdate();
+    console.log(app.currentRoom);
+  });
 
+  $(document).on('click','.username', function() {
+    app.friends.push($(this).text());
+    app.friends = _.uniq(app.friends);
+    console.log(app.friends);
+  });
 });
 
 
@@ -51,7 +60,8 @@ var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
   allMessages: null,
   allRooms: null,
-  currentRoom: null
+  currentRoom: 'All',
+  friends: []
 };
 
 var search = window.location.search;
@@ -73,7 +83,7 @@ app.fetch = function() {
     success: function(data) {
       context.allMessages = data.results;
       //context.roomListUpdate();
-      //context.displayMostRecent();
+      context.displayMostRecent(app.currentRoom);
     },
     error: function(data){
       console.error('Could not get messages.')
@@ -85,25 +95,56 @@ app.fetch = function() {
 app.roomListUpdate = function(){
   $('select > option').remove();
   this.allRooms = _.uniq(_.pluck(this.allMessages, 'roomname'));
-  $('select').append('<option value="All">All</option>');
+  $('select').append('<option value="All" id="All">All</option>');
+  $('select').append('<option value="NEW ROOM" id="NEW ROOM">NEW ROOM</option>')
   for (var i = 0; i < this.allRooms.length; i++) {
     if (this.allRooms[i] !== undefined && this.allRooms[i] !== '') {
-      $('select').append('<option value=' + escapeHtml(this.allRooms[i]) + '>' 
+      $('select').append('<option value=' + escapeHtml(this.allRooms[i]) 
+        + ' id=' + escapeHtml(this.allRooms[i]) + '>' 
         + escapeHtml(this.allRooms[i]) + '</option>');
     }
   }
+  $('#' + app.currentRoom).attr('selected',true);
+
 };
 
 app.displayMostRecent = function(room){
   $('#chats > div').remove();
-  for (var i = 0; i < this.allMessages.length; i++) {
-    if(this.allMessages[i].roomname === room){
-      $('#chats').append('<div class="chat"><span class="username">'
-        + escapeHtml(this.allMessages[i].username) + ': </span><span class="text">' 
-        + escapeHtml(this.allMessages[i].text) + '</span></div>');
+  if (room === 'All'){
+    for (var i = 0; i < this.allMessages.length; i++) {
+      app.append(this.allMessages[i]);
     }
   }
+  else {
+    for (var i = 0; i < this.allMessages.length; i++) {
+      if(this.allMessages[i].roomname === room){
+        app.append(this.allMessages[i]);
+      }
+    }
+  }
+
+  $('.username').each(function(el) {
+    //debugger;
+    if (app.friends.indexOf($(this).text()) !== -1){
+      $(this).parent().toggleClass('friend');
+    }
+  });
 };
+
+app.append = function(message) {
+  $('#chats').append('<div class="chat"><span  class="username">'
+      + escapeHtml(message.username) + '</span>: <span class="text">' 
+      + escapeHtml(message.text) + '</span></div>');
+
+  // var el = '<div class="chat"><span  class="username">'
+  //     + escapeHtml(message.username) + '</span>: <span class="text';
+
+  // if (app.friends.indexOf(message.username) !== -1) {
+  //   el += ' friend';
+  // }
+
+  // el += '">' + escapeHtml(message.text) + '</span></div>';
+}
 
 // Posts new message to chatterbox
 app.send = function(message){
@@ -125,4 +166,4 @@ app.init();
 //debugger;
 setInterval(app.fetch.bind(app), 10000);
 
-setTimeout(app.roomListUpdate.bind(app), 5000);
+setTimeout(app.roomListUpdate.bind(app), 3000);
