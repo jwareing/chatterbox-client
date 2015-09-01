929e72428f57f02512d57042c5c2381f58b4e1c4
jwareing <jwareing89@gmail.com>
2015-08-31 21:03:20 -0700

    Worked on room selection features. Needs more work.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,6 +19,13 @@ $(document).ready(function() {
       return false;
     }
   });
+$('select').on( "change", function(){
+  app.currentRoom = this.value;
+  app.displayMostRecent(app.currentRoom);
+  app.roomListUpdate();
+  console.log(app.currentRoom);
+
+});
 
 });
 
@@ -42,7 +49,9 @@ var escapeHtml = function(string) {
 //Sets up app object, which contains all variables and methods for chatterbox app.
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
-  allMessages: null
+  allMessages: null,
+  allRooms: null,
+  currentRoom: null
 };
 
 var search = window.location.search;
@@ -63,21 +72,36 @@ app.fetch = function() {
     dataType: 'json',
     success: function(data) {
       context.allMessages = data.results;
-      context.displayMostRecent(10);
+      //context.roomListUpdate();
+      //context.displayMostRecent();
     },
     error: function(data){
       console.error('Could not get messages.')
     }
   });
+
 };
 
-// Displays 10 most recent messages in archive fetched from server.
-app.displayMostRecent = function(num){
+app.roomListUpdate = function(){
+  $('select > option').remove();
+  this.allRooms = _.uniq(_.pluck(this.allMessages, 'roomname'));
+  $('select').append('<option value="All">All</option>');
+  for (var i = 0; i < this.allRooms.length; i++) {
+    if (this.allRooms[i] !== undefined && this.allRooms[i] !== '') {
+      $('select').append('<option value=' + escapeHtml(this.allRooms[i]) + '>' 
+        + escapeHtml(this.allRooms[i]) + '</option>');
+    }
+  }
+};
+
+app.displayMostRecent = function(room){
   $('#chats > div').remove();
-  for (var i = 0; i < num; i++) {
-    $('#chats').append('<div class="chat"><span class="username">'
-      + escapeHtml(this.allMessages[i].username) + ': </span><span class="text">' 
-      + escapeHtml(this.allMessages[i].text) + '</span></div>');
+  for (var i = 0; i < this.allMessages.length; i++) {
+    if(this.allMessages[i].roomname === room){
+      $('#chats').append('<div class="chat"><span class="username">'
+        + escapeHtml(this.allMessages[i].username) + ': </span><span class="text">' 
+        + escapeHtml(this.allMessages[i].text) + '</span></div>');
+    }
   }
 };
 
@@ -99,5 +123,6 @@ app.send = function(message){
 
 app.init();
 //debugger;
-setInterval(app.fetch.bind(app), 2000);
+setInterval(app.fetch.bind(app), 10000);
 
+setTimeout(app.roomListUpdate.bind(app), 5000);
