82e686015b4f2b4f17410b61599c92ca8d70afc6
jwareing <jwareing89@gmail.com>
2015-09-01 18:47:33 -0700

    Added room models/views

--- client/scripts/appBB.js
+++ client/scripts/appBB.js
@@ -5,6 +5,21 @@ var allRooms = null;
 var currentRoom = 'All';
 var friends = [];
 
+// HTML ESCAPING
+var entityMap = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': '&quot;',
+  "'": '&#39;',
+  "/": '&#x2F;'
+};
+
+var escapeHtml = function(string) {
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s];
+  })
+};
 
 // MODELS
 var Message = Backbone.Model.extend({
@@ -16,7 +31,11 @@ var Message = Backbone.Model.extend({
   //message?
 });
 
-
+var Room = Backbone.Model.extend({
+  initialize: function(messageObj) {
+    this.set('name', messageObj.text);
+  }
+});
 
 // MODEL COLLECTIONS
 var Messages = Backbone.Collection.extend({
@@ -26,6 +45,10 @@ var Messages = Backbone.Collection.extend({
   model: Message
 });
 
+var Rooms = Backbone.Collection.extend({
+  model: Room
+});
+
 // VIEWS
 var MessageView = Backbone.View.extend({
 
@@ -33,36 +56,48 @@ var MessageView = Backbone.View.extend({
 
   },
 
-  entityMap: {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': '&quot;',
-    "'": '&#39;',
-    "/": '&#x2F;'
-  },
-
-  escapeHtml: function(string) {
-    var context = this;
-    return String(string).replace(/[&<>"'\/]/g, function (s) {
-      return context.entityMap[s];
-    })
-  },
-
   render: function() {
     var html = [
       '<div class="chat">',
         '<span class="username">',
-          this.escapeHtml(this.model.get('username')),
+          escapeHtml(this.model.get('username')),
         '</span>',
         ': ',
         '<span class="text">',
-          this.escapeHtml(this.model.get('message')),
+          escapeHtml(this.model.get('message')),
         '</span>',
       '</div>'
     ].join('');
     
     return this.$el.html(html);
+  }
+});
+
+var RoomView = Backbone.View.extend({
+  initialize: function(){},
+
+  render: function(){
+    var html = [
+      '<option value="',
+        escapeHtml(this.model.get('name')),
+        '">',
+        escapeHtml(this.model.get('name')),
+      '</option>'
+    ].join('');
+
+    return this.$el.html(html);
+  }
+});
+
+var RoomsView = Backbone.View.extend({
+  initialize: function(){
+    this.model.on('change:name', this.render, this)
+  },
+
+  render: function(){
+    $('select').append(this.model.map(function(room){
+      return new RoomView({ model: room }).render();
+    }))
   }
 });
 
@@ -91,6 +126,8 @@ var fetch = function() {
 
       _.each(data.results, function(item){
         allMessages.push(new Message(item));
+        // allRooms.push(new Room(item));
+
       });
 
       // Add all of these to a new collection:
